f529ea22d3efee9fa8c06aea9a66afcd38ed6415
geokira <gakirag@gmail.com>
2015-10-10 15:15:21 -0400

    code to create a project is done

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,24 @@
 class ProjectsController < ApplicationController
   def new
+      @project = Project.new
   end
     
     def show
         @project = Project.find(params[:id])
+    end
+    
+    def create
+        @project = Project.new(project_params)
+        if @project.save
+            # Handle a successful save.
+        else
+            render 'new'
+        end
+    end
+    
+    private
+    
+    def project_params
+        params.require(:project).permit(:name, :project_number_str, :project_description)
     end
 end
