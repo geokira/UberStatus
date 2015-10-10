2d0882ef9afe1f1c3cd5546f6f7b690a7c83068e
geokira <gakirag@gmail.com>
2015-10-10 08:12:40 -0400

    dynamic title

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -4,11 +4,13 @@ class WelcomeControllerTest < ActionController::TestCase
     test "should get index" do
         get :index
         assert_response :success
+        assert_select "title", "Home | Uber Status"
     end
 
     test "should get contact_us" do
         get :contact_us
         assert_response :success
+        assert_select "title", "Contact Us | Uber Status"
     end
 
 end
