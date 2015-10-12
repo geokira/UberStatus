930208f5cc5e0b01975ebfb96c0abe75ca0e6120
geo <gakirag@gmail.com>
2015-10-12 17:39:37 +0000

    partial that shows an individual project plus index.html.erb draft

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,11 +2,11 @@ class ProjectsController < ApplicationController
   def new
       @project = Project.new
   end
-    
+
     def show
         @project = Project.find(params[:id])
     end
-    
+
     def create
         @project = Project.new(project_params)
         if @project.save
@@ -16,9 +16,13 @@ class ProjectsController < ApplicationController
             render 'new'
         end
     end
-    
+
+  def index
+    @projects = Project.all
+  end
+
     private
-    
+
     def project_params
         params.require(:project).permit(:name, :project_number_str, :project_description)
     end
