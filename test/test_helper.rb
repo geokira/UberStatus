e5dd67f06212af159bf721f13bf9170250230951
geo <gakirag@gmail.com>
2015-10-13 17:13:38 +0000

    change coveralls lines to the top of the test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,12 @@
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
 
 # code require to activate coveralls
 require 'coveralls'
 Coveralls.wear!
+
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
