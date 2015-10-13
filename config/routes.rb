81f2a30cf98bd26d3f58dabb45aa64eed037bdda
geo <gakirag@gmail.com>
2015-10-13 16:49:30 +0000

    Users Controller show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-
+  get 'welcome/index' #Home Page
+  get 'contact' => 'welcome#contact_us' #Contact Page
+  
   get 'users/new'
-
-  get 'welcome/index'
-  get 'contact' => 'welcome#contact_us'
-    
   get 'new_project' => 'projects#new'
+
   resources :projects
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
