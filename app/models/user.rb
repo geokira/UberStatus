aa3081d6d8885c9a291bf7b10d114ade2ab91717
geo <gakirag@gmail.com>
2015-10-16 23:47:21 +0000

    added one line of comment to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
+# The User model will authenticate the system users.
+#
 class User < ActiveRecord::Base
-  # Before each save the email address will be downcase to prevent instances of duplicate emails. 
+  # Before each save the email address will be downcase to prevent instances of duplicate emails.
   before_save { email.downcase! }
-  
-  # Name of the user will be 50 charactes or less and it needs to be present. 
+
+  # Name of the user will be 50 charactes or less and it needs to be present.
   validates :name, presence: true, length: {maximum: 50}
-  
-  # Regex will validate email address. 
+
+  # Regex will validate email address.
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255},
     format: {with: VALID_EMAIL_REGEX},uniqueness: { case_sensitive: false }
